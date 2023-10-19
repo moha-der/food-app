59ff068f9b5829d0c4b8b0654c67c6af70845d03
mohammed derfoufi <simo.derfoufi.99@gmail.com>
2023-10-20 01:21:20 +0200

    Creado Footer y Hero para el home

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
 
 import "bootstrap/dist/css/bootstrap.css"
 import 'remixicon/fonts/remixicon.css'
 import { BrowserRouter as Router } from 'react-router-dom'
+
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
